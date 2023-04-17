7dd4cfc4f2aadc6bd69fdbec4b21a205a9ad980a
oddyhater <roman.grishin.me@gmail.com>
2023-04-17 12:04:26 +0600

    html --> jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <body className="page">
+      <header className="header">
+        <a href="#" className="header__link">
+          <img src="<%=require('./images/svg/logo-white.svg')%>" alt="Логотип Место" className="header__logo"/>
         </a>
       </header>
-    </div>
+      <main className="content"> 
+        <section className="profile">
+          <div className="profile__avatar-container">
+            <img src=" " alt="Ваш аватар" className="profile__avatar"/>
+            <div className="profile__avatar-edit"></div>
+          </div>
+          <div className="profile__info">
+            <div className="profile__edit">
+              <h1 className="profile__name"></h1>
+              <button type="button" aria-label="Редактировать профиль" className="profile__edit-button clickable"></button>
+            </div> 
+            <p className="profile__description"></p>
+          </div>
+          <button type="button" aria-label="Добавить карточку" className="profile__add-button clickable"></button>
+        </section>
+        <section className="cards">
+          <ul className="cards__list">
+        
+          </ul>
+        </section>
+      </main>
+      <footer className="footer">
+        <p className="footer__copyright">© 2023 Mesto Russia</p>
+      </footer>
+      <div className="popup" id="popup-profile">
+        <div className="popup__container">
+          <h2 className="popup__title">Редактировать профиль</h2>
+          <form name="edit-profile" className="popup__form" novalidate>
+            <input type="text" name="name" className="popup__input popup__input_type_name" id="profile-name" placeholder="Имя" required minlength="2" maxlength="40"/>
+            <span className="profile-name-error"></span>
+            <input type="text" name="link" className="popup__input popup__input_type_description" id="profile-email" placeholder="О себе" required minlength="2" maxlength="200"/>
+            <span className="profile-email-error"></span>
+            <button type="submit" className="popup__submit">Сохранить</button>
+          </form>
+          <button type="button" aria-label="Закрыть" className="popup__close-button clickable"></button>
+        </div>
+      </div>
+      <div className="popup" id="popup-new-card">
+        <div className="popup__container">
+          <h2 className="popup__title">Новое место</h2>
+          <form name="add-card" className="popup__form" id="popup-add" novalidate>
+            <input type="text" name="name" className="popup__input popup__input_type_name" id="card-name" placeholder="Название" required minlength="2" maxlength="30"/>
+            <span className="card-name-error"></span>
+            <input type="url" name="link" className="popup__input popup__input_type_description" placeholder="Ссылка на картинку" required id="card-url"/>
+            <span className="card-url-error"></span>
+            <button type="submit" className="popup__submit popup__submit_inactive" disabled>Создать</button>
+          </form>
+          <button type="button" aria-label="Закрыть" className="popup__close-button clickable"></button>
+        </div>
+      </div>
+      <div className="popup popup-image" id="popup-image">
+        <div className="popup-image__container popup__container">
+          <figure className="popup-image__picture-set">
+            <img src="#" alt=" " className="popup-image__image"/>
+            <figcaption className="popup-image__caption"></figcaption>
+          </figure>
+          <button type="button" aria-label="Закрыть" className="popup__close-button clickable"></button>
+        </div>
+      </div>
+      <div className="popup popup-delete" id="popup-delete">
+        <div className="popup__container popup-delete__container">
+          <p className="popup-delete__text">Вы уверены?</p>
+          <button type="submit" aria-label="Да" className="popup__submit popup-delete__submit clickable">Да</button>
+          <button type="button" aria-label="Закрыть" className="popup__close-button clickable"></button>
+        </div>
+      </div>
+      <div className="popup popup-edit-avatar" id="popup-edit-avatar">
+        <div className="popup-edit-avatar__container popup__container">
+          <h2 className="popup__title">Обновить аватар</h2>
+          <form name="edit-avatar" className="popup__form" id="popup-edit" novalidate>
+            <input type="url" name="link" className="popup__input popup__input_type_description" placeholder="Ссылка на картинку" required id="avatar-link"/>
+            <span className="avatar-link-error"></span>
+            <button type="submit" className="popup__submit popup__submit_inactive" disabled>Сохранить</button>
+          </form>
+          <button type="button" aria-label="Закрыть" className="popup__close-button clickable"></button>
+        </div>
+      </div>
+      <template className="template">
+        <li className="card">
+          <img src="#" alt=" " className="card__image"/>
+          <div className="card__info">
+            <h3 className="card__name"></h3>
+            <div className="card__like-wrapper">
+              <button type="button" aria-label="Лайк" className="card__like"></button>
+              <span className="card__like-number">0</span>
+            </div>
+          </div>
+          <button type="button" aria-label="Удалить карточку" className="card__trash-button clickable"></button>
+        </li>
+      </template>
+    </body>
   );
 }
 
